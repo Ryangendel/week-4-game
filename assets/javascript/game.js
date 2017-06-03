697e462da667ad257c25538c945c9a6261cbe709
Ryan Gendel <ryangendel@Ryans-MacBook-Pro-2.local>
2017-06-02 19:08:50 -0500

    Updating

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// Variables
+
+var crystal1=[],
+var crystal2=[],
+var crystal3=[],
+var crystal4=[],
+var totalScore = [],
+var runningTotalScore=[],
+var wins=0,
+var losses=0,
+var randomTargetNumber =[],
+var userScore=
+var currentScore=
+var usersCurrentScore=
+
+randomTargetNumber
+
+// Events
+
+
+
+
+// Functions
---
+++ game.js
@@ -0,0 +1,67 @@
+//varables
+console.log("hekko")
+
+$(document).ready(function(){
+	var Random=Math.floor(Math.random()*101+19)
+
+}
+
+
+// $ ('#randomTargetNumber').text(Random);
+
+// var cry1=Math.floor(Math.random()*11+1)
+// var cry2=Math.floor(Math.random()*11+1)
+// var cry3=Math.floor(Math.random()*11+1)
+// var cry4=Math.floor(Math.random()*11+1)
+
+// var wins=0;
+// var losses=0;
+// var currentScore=0;
+
+// //events
+
+// $('#numberWins').text(wins);
+// $('#numberLosses').text(losses);
+
+// function reset(){
+// 	Random=Math.floor(Math.random()*101+19);
+// 	console.log(Random)
+// 	$('#randomTargetNumber').text(Random);
+// 	cry1=Math.floor(Math.random()*11+1);
+// 	cry2=Math.floor(Math.random()*11+1);
+// 	cry3=Math.floor(Math.random()*11+1);
+// 	cry4=Math.floor(Math.random()*11+1);
+// 	currentScore=0;
+// 	$('#userCurrentScore').text(currentScore);
+// }
+// 	function win(){
+// 		alert("Way to go!");
+// 		wins++;
+// 		$('#totalWins').text(wins);
+// 		reset();
+// 	}
+
+// 	function lost(){
+// 		alert("too bad");
+// 		losses++;
+// 		$('#totalLosses').text(losses);
+// 		reset()
+// 	}
+
+// //jewel click set up
+
+// $("#crystal1").on('click',function(){
+// 	console.log("hello")
+// 	currentScore= currentScore + cry1;
+// 	console.log("new usertotal = " + currentScore);
+// 	$('#totalScore').text(currentScore);
+// 		if (currentScore==Random){
+// 			win();
+// 		}
+// 		else if(currentScore>Random){
+// 			lost();
+// 		}	
+// 	}
+// })
+
+
