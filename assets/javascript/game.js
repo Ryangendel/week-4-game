14d404ee7fe622379d65b69b7df09cbcb8066c12
Ryan Gendel <ryangendel@Ryans-MacBook-Pro-2.local>
2017-06-03 05:38:21 -0500

    almost everything is working but still some stuff i need to figure out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,105 @@
 //varables
-console.log("hekko");
+
+
+// $('#randomTargetNumber').text(Random);
+var Random=0;
+var cry1=Math.floor(Math.random()*11+1)
+var cry2=Math.floor(Math.random()*11+1)
+var cry3=Math.floor(Math.random()*11+1)
+var cry4=Math.floor(Math.random()*11+1)
+
+var wins=0;
+var losses=0;
+var currentScore=0;
+
+
+//events
 
 $(document).ready(function(){
-	var Random=Math.floor(Math.random()*101+19)
+	var Random=Math.floor(Math.random()*101+19);
+	$('#randomTargetNumber').html("Number To Match:" + Random);
 
+})
+
+$('#totalWins').text(wins);
+$('#totalLosses').text(losses);
+
+var reset = function(){
+	var Random=Math.floor(Math.random()*101+19);
+	console.log(Random)
+	$('#randomTargetNumber').text(Random);
+	cry1=Math.floor(Math.random()*11+1);
+	cry2=Math.floor(Math.random()*11+1);
+	cry3=Math.floor(Math.random()*11+1);
+	cry4=Math.floor(Math.random()*11+1);
+	currentScore===0;
+	$('#usersCurrentScore').text(currentScore);
 }
 
+	var win =function(){
+		console.log('test');
+		alert('Way to go!');
+		wins++;
+		$('#totalWins').text(wins);
+		reset();
+	}
 
-// $ ('#randomTargetNumber').text(Random);
-
-// var cry1=Math.floor(Math.random()*11+1)
-// var cry2=Math.floor(Math.random()*11+1)
-// var cry3=Math.floor(Math.random()*11+1)
-// var cry4=Math.floor(Math.random()*11+1)
-
-// var wins=0;
-// var losses=0;
-// var currentScore=0;
-
-// //events
-
-// $('#numberWins').text(wins);
-// $('#numberLosses').text(losses);
-
-// function reset(){
-// 	Random=Math.floor(Math.random()*101+19);
-// 	console.log(Random)
-// 	$('#randomTargetNumber').text(Random);
-// 	cry1=Math.floor(Math.random()*11+1);
-// 	cry2=Math.floor(Math.random()*11+1);
-// 	cry3=Math.floor(Math.random()*11+1);
-// 	cry4=Math.floor(Math.random()*11+1);
-// 	currentScore=0;
-// 	$('#userCurrentScore').text(currentScore);
-// }
-// 	function win(){
-// 		alert("Way to go!");
-// 		wins++;
-// 		$('#totalWins').text(wins);
-// 		reset();
-// 	}
-
-// 	function lost(){
-// 		alert("too bad");
-// 		losses++;
-// 		$('#totalLosses').text(losses);
-// 		reset()
-// 	}
+	var lost= function (){
+		console.log("text");
+		alert('too bad');
+		losses++;
+		$('#totalLosses').text(losses);
+		reset();
+	}
 
 // //jewel click set up
 
-// $("#crystal1").on('click',function(){
-// 	console.log("hello")
-// 	currentScore= currentScore + cry1;
-// 	console.log("new usertotal = " + currentScore);
-// 	$('#totalScore').text(currentScore);
-// 		if (currentScore==Random){
-// 			win();
-// 		}
-// 		else if(currentScore>Random){
-// 			lost();
-// 		}	
-// 	}
-// })
+$("#crystal1").on('click',function(){
+	currentScore= currentScore + cry1;
+	console.log("new usertotal = " + currentScore);
+	$('#totalScore').text(currentScore);
+		if (currentScore===Random){
+			win();
+		}
+		else if(currentScore<Random){
+			lost();
+		}	
+	})
+
+$("#crystal2").on('click',function(){
+	currentScore= currentScore + cry2;
+	console.log("new usertotal = " + currentScore);
+	$('#totalScore').text(currentScore);
+		if (currentScore===Random){
+			win();
+		}
+		else if(currentScore<Random){
+			lost();
+		}	
+	})
+
+$("#crystal3").on('click',function(){
+	currentScore= currentScore + cry3;
+	console.log("new usertotal = " + currentScore);
+	$('#totalScore').text(currentScore);
+		if (currentScore===Random){
+			win();
+		}
+		else if(currentScore<Random){
+			lost();
+		}	
+	})
+
+$("#crystal4").on('click',function(){
+	currentScore= currentScore + cry4;
+	console.log("new usertotal = " + currentScore);
+	$('#totalScore').text(currentScore);
+		if (currentScore===Random){
+			win();
+		}
+		else if(currentScore<Random){
+			lost();
+		}	
+	})
 
 
